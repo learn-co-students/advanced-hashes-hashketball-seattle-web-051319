669e4513b92c55d8e23c18fa47bde17729062d8c
Bryna Dunnells <badunnells@gmail.com>
2019-05-10 03:40:58 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -110,7 +110,7 @@ def num_points_scored (player)
     game_hash.collect do |location, data|
       data.each do |k, v|
         if k == :players
-          :players.find(player)
+          players.find(player)
           player.fetch(:points)
         end
       end
